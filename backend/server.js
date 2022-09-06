8dc36fbb0d486bdedf6f91b4faa4293118a33c89
sinoom <sinoom@ttu.ee>
2022-09-06 20:55:35 +0300

    backend api logic

---
+++ backend/server.js
@@ -0,0 +1,87 @@
+const e = require("express");
+const express = require("express");
+
+const app = express();
+app.use(express.json());
+
+const database = {
+  users: [
+    {
+      id: "123",
+      name: "John",
+      email: "john@gmail.com",
+      password: "cookies",
+      entries: 0,
+      joined: new Date(),
+    },
+    {
+      id: "321",
+      name: "Sally",
+      email: "sally@gmail.com",
+      password: "oranges",
+      entries: 0,
+      joined: new Date(),
+    },
+  ],
+};
+
+app.get("/", (req, res) => {
+  res.json(database.users);
+});
+
+app.post("/signin", (req, res) => {
+  if (
+    req.body.email === database.users[0].email &&
+    req.body.password === database.users[0].password
+  ) {
+    res.json("success");
+  } else {
+    res.json("error logging in");
+  }
+});
+
+app.post("/signup", (req, res) => {
+  const { email, name, password } = req.body;
+  database.users.push({
+    id: "1323",
+    name: name,
+    email: email,
+    password: password,
+    entries: 0,
+    joined: new Date(),
+  });
+  res.json(database.users[database.users.length - 1]);
+});
+
+app.get("/profile/:id", (req, res) => {
+  const { id } = req.params;
+  let found = false;
+  database.users.forEach((user) => {
+    if (user.id === id) {
+      found = true;
+      return res.json(user);
+    }
+  });
+  if (!found) {
+    res.status(400).json("user not found");
+  }
+});
+
+app.put("/image", (req, res) => {
+  const { id } = req.body;
+  let found = false;
+  database.users.forEach((user) => {
+    if (user.id === id) {
+      found = true;
+      user.entries++;
+      return res.json(user.entries);
+    }
+  });
+  if (!found) {
+    res.status(400).json("user not found");
+  }
+});
+
+app.listen(3000, () => {
+  console.log("app is running on port 3000");
+});
